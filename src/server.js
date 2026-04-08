c892184f4d2ee058cf55b9f5b017523f53866bd4
alessiaquinti <aleq9893@gmail.com>
2026-04-08 12:20:07 +0200

    Abilitato CORS per frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
 const app = express();
+
+
+app.use(cors({
+  origin: "http://localhost:5173" // permette solo il tuo frontend locale
+}));
 
 app.use(express.json()); // IMPORTANTISSIMO
 
