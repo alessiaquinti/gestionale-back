d033a385bf7c816b0da183483cca618213f73080
alessiaquinti <aleq9893@gmail.com>
2026-04-08 14:16:04 +0200

    Remove app.options(*) fix PathError

--- src/server.js
+++ src/server.js
@@ -15,8 +15,7 @@ app.use(cors({
 app.use(express.json());
 app.use("/auth", authRoutes);
 
-// risposta rapida per il preflight OPTIONS (opzionale ma sicura)
-app.options("*", cors());
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
